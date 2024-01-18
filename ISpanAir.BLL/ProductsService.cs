7338a973b4b798845dbdd99a4f3efde2cd8bde3d
happyachi <gn77233@gmail.com>
2024-01-18 23:32:38 +0800

    加入專案檔案。

---
+++ ISpanAir.BLL/ProductsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISpanAir.BLL
+{
+	internal class ProductsService
+	{
+	}
+}
