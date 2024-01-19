06438b9bd53bb9ca1d66d3803b314455c0751ca7
Cheerscheer <chiya951@gmail.com>
2024-01-19 11:26:40 +0800

    #Random alphabet

---
+++ ISpanAir.Entities/Cheertest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISpanAir.Entities
+{
+	public static class Cheertest
+	{
+		public static string GenerateRandomLetters(int length)
+		{
+			Random random = new Random();
+			const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+			char[] letters = new char[length];
+
+			for (int i = 0; i < length; i++)
+			{
+				int index = random.Next(alphabet.Length);
+				letters[i] = alphabet[index];
+			}
+
+			return new string(letters);
+		}
+	}
+}
